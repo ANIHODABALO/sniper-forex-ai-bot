cc55e0b198e0ec49a7ff9f88202161e1af10d927
ANIHODABALO <hodabaloani983@gmail.com>
2026-05-25 07:35:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("SNIPER FOREX AI BOT STARTED")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "SNIPER FOREX AI BOT IS RUNNING"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
