1f321180b725cfe0a93d2b055d489ec59a4e28ee
ANIHODABALO <hodabaloani983@gmail.com>
2026-05-30 18:04:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -99,9 +99,7 @@ def home():
             buy_tp = resistance
             sell_tp = support
             candle_size = abs(current_close - current_open)
-# Entrée Théorique
-buy_entry = current_ema50
-sell_entry = current_ema50
+
             avg_candle_size = (
                 abs(close_prices.iloc[-6:-1] - open_prices.iloc[-6:-1])
             ).mean()
