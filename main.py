147398e6803bdabe4c81d3c772eaf28fb91d3a47
ANIHODABALO <hodabaloani983@gmail.com>
2026-06-02 10:52:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -103,6 +103,8 @@ def home():
             # Bougie actuelle
             current_open = open_prices.iloc[-1]
             current_close = close_prices.iloc[-1]
+            current_high = high_prices.iloc[-1]
+current_low = low_prices.iloc[-1]
 # TP Zone Clé
             buy_tp = resistance
             sell_tp = support
